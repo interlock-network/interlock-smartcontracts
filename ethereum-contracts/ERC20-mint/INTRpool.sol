89a8312a5f97edf5ce53075b7a752efcf45a2ba7
Blair <blairmunroakusa@wp.computer>
2022-05-04 18:28:29 -0800

    cleaned out INTRpool satellite contract

--- ethereum-contracts/ERC20-mint/INTRpool.sol
+++ ethereum-contracts/ERC20-mint/INTRpool.sol
@@ -8,20 +8,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./ERC20INTR.sol";
-
 contract INTRpool {
-
-    address _mintAddress;
-    ERC20INTR public mint;
-
-    constructor (address mintAddress_) {
-        _mintAddress = mintAddress_;
-        mint = ERC20INTR(_mintAddress); }
-
-	function splitPool(uint256 share, address member) public {
-        require(mint.balanceOf(member) == 0, "already added");
-        mint.approve(member, share); }
 
 }
 	
