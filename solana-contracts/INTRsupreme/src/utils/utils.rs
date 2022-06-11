e73dc9d1bbd85cf19bf592e23e72e931fdf7efce
Blair <blairmunroakusa@wp.computer>
2022-06-11 13:18:09 -0800

    implement u16 flag pack/uppack

--- solana-contracts/INTRsupreme/src/utils/utils.rs
+++ solana-contracts/INTRsupreme/src/utils/utils.rs
@@ -54,6 +54,25 @@ pub fn unpack_32_flags(flags: u32) -> BitVec {
     return flagbits
 }
 
+// pack flags into a single u16
+pub fn pack_flags(flags: BitVec) -> u16 {
+
+    let flagbytes = BitVec::to_bytes(&flags);
+    let bigflag = ((flagbytes[0] as u16) << 8) | flagbytes[1] as u16;
+
+    return bigflag
+}
+
+// unpack flags from a single u16
+pub fn unpack_flags(flags: u16) -> BitVec {
+
+    let highflag: u8 = (flags >> 8) as u8;
+    let lowflag: u8 = (flags & 0xff) as u8;
+    let flagbits = BitVec::from_bytes(&[highflag, lowflag]);
+
+    return flagbits
+}
+
 
 
 //  pack a string into fixed size byte array:w
