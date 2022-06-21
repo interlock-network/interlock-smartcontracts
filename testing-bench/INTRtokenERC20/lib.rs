e78ef34af7f762a8c266fb6ab10083fef6248138
Blair <blairmunroakusa@wp.computer>
2022-06-20 17:08:02 -0800

    Substrate: full erc20 implementation, tested

--- testing-bench/INTRtokenERC20/lib.rs
+++ testing-bench/INTRtokenERC20/lib.rs
@@ -25,30 +25,77 @@ mod INTRtokenERC20 {
     #[derive(SpreadAllocate)]
     pub struct IntRtokenErc20 {
         /// Stores a single `bool` value on the storage.
+        decimals: u8,
         total_supply: u32,
         balances: Mapping<AccountId, u32>,
+        allowances: Mapping<(AccountId, AccountId), u32>,
+    }
+
+
+    /// Specify ERC-20 error type.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Return if the balance cannot fulfill a request.
+        InsufficientBalance,
+    }
+
+    /// Specify the ERC-20 result type.
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    /// transfer event
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        amount: u32,
+    }
+
+    /// approve event
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        amount: u32,
     }
 
     use ink_lang::utils::initialize_contract;
     use ink_storage::Mapping;
     impl IntRtokenErc20 {
 
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-
+        /// Constructor that initializes the `bool` value to the given `init_value`
         #[ink(constructor)]
         pub fn new_token(supply: u32) -> Self {
             initialize_contract(|contract: &mut Self| {
                 let caller = Self::env().caller();
                 contract.balances.insert(&caller, &supply);
                 contract.total_supply = supply;
+                Self::env().emit_event(Transfer {
+                    from: None,
+                    to: Some(caller),
+                    amount: supply,
+                });
+                contract.decimals = 18;
             })
         }
 
+        /// token decimal count getter
+        #[ink(message)]
+        pub fn decimals(&self) -> u8 {
+            self.decimals
+        }
+
+        /// total supply getter
         #[ink(message)]
         pub fn total_supply(&self) -> u32 {
             self.total_supply
         }
 
+        /// account balance getter
         #[ink(message)]
         pub fn balance_of(&self, account: AccountId) -> u32 {
             match self.balances.get(&account) {
@@ -57,6 +104,16 @@ mod INTRtokenERC20 {
             }
         }
 
+        /// account allowance getter
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> u32 {
+            match self.allowances.get((&owner, &spender)) {
+                Some(value) => value,
+                None => 0,
+            }
+        }
+        
+        /// transfer method
         #[ink(message)]
         pub fn transfer(&mut self, recipient: AccountId, amount: u32) -> bool {
             let sender = self.env().caller();
@@ -71,7 +128,42 @@ mod INTRtokenERC20 {
             true
         }
 
+        /// approve method
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, amount: u32) -> bool {
+            let owner = self.env().caller();
+            // here, make sure owner and spender addresses are not 0 address
+            self.allowances.insert((&owner, &spender), &amount);
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                amount,
+            });
+            true
+        }
+
+        /// transfer from method
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            amount: u32,
+        ) -> Result<()> {
+            let from_balance = self.balance_of(from);
+            if from_balance < amount {
+                return Err(Error::InsufficientBalance)
+            }
+            self.balances.insert(from, &(from_balance - amount));
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, &(to_balance + amount));
+            Ok(())
+        }
+
     }
+
+/////////////////////////////////////////////////////////////////////////
+
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
