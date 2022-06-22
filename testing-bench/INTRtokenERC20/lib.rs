79b51851879899786c6a918830098f50ba8e4b37
Blair <blairmunroakusa@wp.computer>
2022-06-22 14:46:51 -0800

    ink! ERC20: affirmative unit tests written

--- testing-bench/INTRtokenERC20/lib.rs
+++ testing-bench/INTRtokenERC20/lib.rs
@@ -65,9 +65,9 @@ mod INTRtokenERC20 {
     #[ink(event)]
     pub struct Approval {
         #[ink(topic)]
-        owner: AccountId,
+        owner: Option<AccountId>,
         #[ink(topic)]
-        spender: AccountId,
+        spender: Option<AccountId>,
         amount: u32,
     }
 
@@ -87,7 +87,7 @@ mod INTRtokenERC20 {
                     to: Some(caller),
                     amount: supply,
                 });
-                contract.name = "test".to_string();
+                contract.name = "Interlock Network".to_string();
                 contract.symbol = "INTR".to_string();
                 contract.decimals = 18;
             })
@@ -147,6 +147,11 @@ mod INTRtokenERC20 {
             let recipient_balance = self.balance_of(recipient);
             self.balances.insert(sender, &(&sender_balance - amount));
             self.balances.insert(recipient, &(&recipient_balance + amount));
+            Self::env().emit_event(Transfer {
+                from: Some(sender),
+                to: Some(recipient),
+                amount: amount,
+            });
             true
         }
 
@@ -157,8 +162,8 @@ mod INTRtokenERC20 {
             // here, make sure owner and spender addresses are not 0 address
             self.allowances.insert((&owner, &spender), &amount);
             self.env().emit_event(Approval {
-                owner,
-                spender,
+                owner: Some(owner),
+                spender: Some(spender),
                 amount,
             });
             true
@@ -171,15 +176,20 @@ mod INTRtokenERC20 {
             from: AccountId,
             to: AccountId,
             amount: u32,
-        ) -> Result<()> {
+        ) -> bool {
             let from_balance = self.balance_of(from);
             if from_balance < amount {
-                return Err(Error::InsufficientBalance)
+                return false
             }
             self.balances.insert(from, &(from_balance - amount));
             let to_balance = self.balance_of(to);
             self.balances.insert(to, &(to_balance + amount));
-            Ok(())
+            Self::env().emit_event(Transfer {
+                from: Some(from),
+                to: Some(to),
+                amount: amount,
+            });
+            true
         }
 
     }
@@ -197,21 +207,287 @@ mod INTRtokenERC20 {
 
         /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
+        use ink_env::Clear;
+        use ink_env::topics::PrefixedValue;
+
+        type Event = <IntRtokenErc20 as ::ink_lang::reflect::ContractEventBase>::Type;
+
 
         /// We test if the default constructor does its job.
+
         #[ink::test]
-        fn default_works() {
-            let INTRtokenERC20 = IntRtokenErc20::default();
-            assert_eq!(INTRtokenERC20.get(), false);
+        fn constructor_works() {
+            let INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+
+            // Transfer event triggered during initial construction.
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(1, emitted_events.len());
+            assert_transfer_event(
+                &emitted_events[0],
+                None,
+                Some(AccountId::from([0x01; 32])),
+                100000,
+            );
+
+            assert_eq!(INTRtokenERC20.name(), "Interlock Network");
+            assert_eq!(INTRtokenERC20.symbol(), "INTR");
+            assert_eq!(INTRtokenERC20.decimals(), 18);
+            assert_eq!(INTRtokenERC20.total_supply(), 100_000);
+
         }
 
-        /// We test a simple use case of our contract.
         #[ink::test]
-        fn it_works() {
-            let mut INTRtokenERC20 = IntRtokenErc20::new(false);
-            assert_eq!(INTRtokenERC20.get(), false);
-            INTRtokenERC20.flip();
-            assert_eq!(INTRtokenERC20.get(), true);
+        fn name_works() {
+            let INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            assert_eq!(INTRtokenERC20.name(), "Interlock Network");
+        }
+
+        #[ink::test]
+        fn symbol_works() {
+            let INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            assert_eq!(INTRtokenERC20.symbol(), "INTR");
+        }
+        
+        #[ink::test]
+        fn decimals_works() {
+            let INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            assert_eq!(INTRtokenERC20.decimals(), 18);
+        }
+
+        #[ink::test]
+        fn total_supply_works() {
+            let INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            assert_eq!(INTRtokenERC20.total_supply(), 100_000);
+        }
+        #[ink::test]
+        fn balance_of_works() {
+            let INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+
+            // Alice owns all the tokens on contract instantiation
+            assert_eq!(INTRtokenERC20.balance_of(accounts.alice), 100_000);
+
+            // Bob does not owns tokens
+            assert_eq!(INTRtokenERC20.balance_of(accounts.bob), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let mut INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+
+            // Alice transfers 10 tokens to Bob.
+            assert_eq!(INTRtokenERC20.transfer(accounts.bob, 10), true);
+            assert_eq!(INTRtokenERC20.balance_of(accounts.alice), 99_990);
+            assert_eq!(INTRtokenERC20.balance_of(accounts.bob), 10);
+
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(emitted_events.len(), 2);
+
+            // Check the transfer event relating to the actual trasfer.
+            assert_transfer_event(
+                &emitted_events[1],
+                Some(AccountId::from([0x01; 32])),
+                Some(AccountId::from([0x02; 32])),
+                10,
+            );
+        }
+
+        #[ink::test]
+        fn approve_works() {
+            let mut INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+
+            // Alice approves bob to spend 10 tokens
+            assert_eq!(INTRtokenERC20.approve(accounts.bob, 10), true);
+            assert_eq!(INTRtokenERC20.allowance(accounts.alice, accounts.bob), 10);
+
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(emitted_events.len(), 2);
+
+            // Check the transfer event relating to the actual trasfer.
+            assert_approval_event(
+                &emitted_events[1],
+                Some(AccountId::from([0x01; 32])),
+                Some(AccountId::from([0x02; 32])),
+                10,
+            );
+        }
+
+        #[ink::test]
+        fn allowance_works() {
+            let mut INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+
+            assert_eq!(INTRtokenERC20.allowance(accounts.alice, accounts.bob), 0);
+            assert_eq!(INTRtokenERC20.approve(accounts.bob, 10), true);
+            assert_eq!(INTRtokenERC20.allowance(accounts.alice, accounts.bob), 10);
+        }
+
+        #[ink::test]
+        fn transfer_from_works() {
+            let mut INTRtokenERC20 = IntRtokenErc20::new_token(100_000);
+            let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+
+            // Set the contract as callee and Bob as caller.
+            let contract = ink_env::account_id::<ink_env::DefaultEnvironment>();
+            ink_env::test::set_callee::<ink_env::DefaultEnvironment>(contract);
+            ink_env::test::set_caller::<ink_env::DefaultEnvironment>(accounts.bob);
+
+            // Alice approves Bob for token transfers on her behalf.
+            assert_eq!(INTRtokenERC20.approve(accounts.bob, 10), true);
+
+            // Bob transfers tokens from Alice to Eve.
+            assert_eq!(
+                INTRtokenERC20.transfer_from(accounts.alice, accounts.eve, 10),
+                true
+            );
+            // Eve owns tokens.
+            assert_eq!(INTRtokenERC20.balance_of(accounts.eve), 10);
+
+            // Check all transfer events that happened during the previous calls:
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(emitted_events.len(), 3);
+
+            // The second event `emitted_events[1]` is an Approve event that we skip checking.
+            assert_transfer_event(
+                &emitted_events[2],
+                Some(AccountId::from([0x01; 32])),
+                Some(AccountId::from([0x05; 32])),
+                10,
+            );
+        }
+
+        /// check that a transfer event is good
+        fn assert_transfer_event(
+            event: &ink_env::test::EmittedEvent,
+            expected_from: Option<AccountId>,
+            expected_to: Option<AccountId>,
+            expected_amount: u32,
+        ) {
+            let decoded_event = <Event as scale::Decode>::decode(&mut &event.data[..])
+                .expect("encountered invalid contract event data buffer");
+            if let Event::Transfer(Transfer { from, to, amount }) = decoded_event {
+                assert_eq!(from, expected_from, "encountered invalid Transfer.from");
+                assert_eq!(to, expected_to, "encountered invalid Transfer.to");
+                assert_eq!(amount, expected_amount, "encountered invalid Trasfer.amount");
+            } else {
+                panic!("encountered unexpected event kind: expected a Transfer event")
+            }
+            let expected_topics = vec![
+                encoded_into_hash(&PrefixedValue {
+                    value: b"IntRtokenErc20::Transfer",
+                    prefix: b"",
+                }),
+                encoded_into_hash(&PrefixedValue {
+                    prefix: b"IntRtokenErc20::Transfer::from",
+                    value: &expected_from,
+                }),
+                encoded_into_hash(&PrefixedValue {
+                    prefix: b"IntRtokenErc20::Transfer::to",
+                    value: &expected_to,
+                }),
+                encoded_into_hash(&PrefixedValue {
+                    prefix: b"IntRtokenErc20::Transfer::amount",
+                    value: &expected_amount,
+                }),
+            ];
+
+            let topics = event.topics.clone();
+            for (n, (actual_topic, expected_topic)) in
+                topics.iter().zip(expected_topics).enumerate()
+            {
+                let mut topic_hash = Hash::clear();
+                let len = actual_topic.len();
+                topic_hash.as_mut()[0..len].copy_from_slice(&actual_topic[0..len]);
+
+                assert_eq!(
+                    topic_hash, expected_topic,
+                    "encountered invalid topic at {}",
+                    n
+                );
+            }
+        }
+
+        /// check that a approval event is good
+        fn assert_approval_event(
+            event: &ink_env::test::EmittedEvent,
+            expected_owner: Option<AccountId>,
+            expected_spender: Option<AccountId>,
+            expected_amount: u32,
+        ) {
+            let decoded_event = <Event as scale::Decode>::decode(&mut &event.data[..])
+                .expect("encountered invalid contract event data buffer");
+            if let Event::Approval(Approval { owner, spender, amount }) = decoded_event {
+                assert_eq!(owner, expected_owner, "encountered invalid Approval.owner");
+                assert_eq!(spender, expected_spender, "encountered invalid Approval.spender");
+                assert_eq!(amount, expected_amount, "encountered invalid Approval.amount");
+            } else {
+                panic!("encountered unexpected event kind: expected a Approval event")
+            }
+            let expected_topics = vec![
+                encoded_into_hash(&PrefixedValue {
+                    value: b"IntRtokenErc20::Approval",
+                    prefix: b"",
+                }),
+                encoded_into_hash(&PrefixedValue {
+                    prefix: b"IntRtokenErc20::Approval::owner",
+                    value: &expected_owner,
+                }),
+                encoded_into_hash(&PrefixedValue {
+                    prefix: b"IntRtokenErc20::Approval::spender",
+                    value: &expected_spender,
+                }),
+                encoded_into_hash(&PrefixedValue {
+                    prefix: b"IntRtokenErc20::Approval::amount",
+                    value: &expected_amount,
+                }),
+            ];
+
+            let topics = event.topics.clone();
+            for (n, (actual_topic, expected_topic)) in
+                topics.iter().zip(expected_topics).enumerate()
+            {
+                let mut topic_hash = Hash::clear();
+                let len = actual_topic.len();
+                topic_hash.as_mut()[0..len].copy_from_slice(&actual_topic[0..len]);
+
+                assert_eq!(
+                    topic_hash, expected_topic,
+                    "encountered invalid topic at {}",
+                    n
+                );
+            }
+        }
+
+
+        fn encoded_into_hash<T>(entity: &T) -> Hash
+        where
+            T: scale::Encode,
+        {
+            use ink_env::{
+                hash::{
+                    Blake2x256,
+                    CryptoHash,
+                    HashOutput,
+                },
+                Clear,
+            };
+            let mut result = Hash::clear();
+            let len_result = result.as_ref().len();
+            let encoded = entity.encode();
+            let len_encoded = encoded.len();
+            if len_encoded <= len_result {
+                result.as_mut()[..len_encoded].copy_from_slice(&encoded);
+                return result
+            }
+            let mut hash_output =
+                <<Blake2x256 as HashOutput>::Type as Default>::default();
+            <Blake2x256 as CryptoHash>::hash(&encoded, &mut hash_output);
+            let copy_len = core::cmp::min(hash_output.len(), len_result);
+            result.as_mut()[0..copy_len].copy_from_slice(&hash_output[0..copy_len]);
+            result
         }
     }
+
 }
