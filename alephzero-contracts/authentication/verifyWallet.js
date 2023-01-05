cb2db731d1f126232ba8bf8b6d8b69ebb690d8fb
Blair <blairmunroakusa@wp.computer>
2023-01-05 13:37:00 -0900

    authenticate/*.js: fix indentation and clean up titles

--- alephzero-contracts/authentication/verifyWallet.js
+++ alephzero-contracts/authentication/verifyWallet.js
@@ -1,41 +1,6 @@
 //
-// INTERLOCK NETWORK - 
+// INTERLOCK NETWORK - VERIFY WALLET
 // PSP34 ACCESS NFT AUTHENTICATION
-//
-
-//
-// AUTHENTICATION FLOW
-//
-// authenticateInterlocker.js		PERSISTENT NODE <-------+
-// | | | | ... |						|	
-// V V V V     V						|
-// verifyWallet.js (amount, wallet) 	CHILD NODES		| return message
-//					(1 per authentication)  |	
-//								|
-//	1) get wallet acces NFT collection			|
-//		--> if// no collection 		then//----------+ 'no NFT held'
-//
-//	2) check authentication status of collection
-//		* loop through collection
-//		--> if// nft isauthenticated 	then// advance
-//		    if// nft notauthenticated	then// note ID
-//		* after loop
-//		--> if// all authenticated	then//----------+ 'NFTs already authenticated'
-//								  (NEED TO XCHECK W DB THEN)
-//	3) begin authentication process
-//		* set up event lister
-//
-//	4) send authentication transaction
-//		* transfer(amount, wallet)
-//		--> if// transfer heard		then//----------+ 'authentication transfer success'
-//
-//	5) wait for verification transfer
-//		--> if// timeout		then//----------+ 'verification transfer timeout'
-//		--> if// transfer heard		then//----------+ 'verification transfer success'
-//
-//	6) setAuthenticated(ID)
-//		--> if// error			then//----------+ 'set authentication fail, error'
-//		--> if// finalized		then//----------+ 'NFT authentication success'
 //
 
 // imports
@@ -55,80 +20,80 @@ const ISAUTHENTICATED = '0x697361757468656e74696361746564';
 
 async function main(message) {
 
-	try {
-		// setup session
-		const wsProvider = new WsProvider('wss://ws.test.azero.dev');
-		const keyring = new Keyring({type: 'sr25519'});
-		const api = await ApiPromise.create({ provider: wsProvider });
-		const contract = new ContractPromise(api, access_metadata, access_contract);
-		const OWNER_pair = keyring.addFromUri(OWNER_mnemonic);
+  try {
+    // setup session
+    const wsProvider = new WsProvider('wss://ws.test.azero.dev');
+    const keyring = new Keyring({type: 'sr25519'});
+    const api = await ApiPromise.create({ provider: wsProvider });
+    const contract = new ContractPromise(api, access_metadata, access_contract);
+    const OWNER_pair = keyring.addFromUri(OWNER_mnemonic);
 
-		let notAuthenticated = false;
-		let notAuthenticatedId;
+    let notAuthenticated = false;
+    let notAuthenticatedId;
 
-		// get NFT collection for wallet
-		let { gasRequired, storageDeposit, result, output } =
-			await contract.query['ilockerCollection'](
-  			OWNER_pair.address, {}, message.wallet);
+    // get NFT collection for wallet
+    let { gasRequired, storageDeposit, result, output } =
+      await contract.query['ilockerCollection'](
+        OWNER_pair.address, {}, message.wallet);
 
-		// check if the call was successful
-		if (result.isOk) {
+    // check if the call was successful
+    if (result.isOk) {
 
-			// find nft to authenticate
-			const collection = JSON.parse(JSON.stringify(output));
-			for (nft in collection.ok) {
+      // find nft to authenticate
+      const collection = JSON.parse(JSON.stringify(output));
+      for (nft in collection.ok) {
 
-				let { gasRequired, storageDeposit, result, output } =
-					await contract.query['psp34Metadata::getAttribute'](
-  					OWNER_pair.address, {}, {u64: collection.ok[nft].u64}, ISAUTHENTICATED);
-				let authenticated = JSON.parse(JSON.stringify(output));
-				if (FALSE == authenticated) {
-					notAuthenticated = true;
-					notAuthenticatedId = collection.ok[nft].u64;
-				}
-			}
+        let { gasRequired, storageDeposit, result, output } =
+          await contract.query['psp34Metadata::getAttribute'](
+            OWNER_pair.address, {}, {u64: collection.ok[nft].u64}, ISAUTHENTICATED);
+        let authenticated = JSON.parse(JSON.stringify(output));
+        if (FALSE == authenticated) {
+          notAuthenticated = true;
+          notAuthenticatedId = collection.ok[nft].u64;
+        }
+      }
 
-			// not nfts to authenticate
-			if (notAuthenticated == false) {
-				process.send('nfts already authenticated')
-				process.exit();
+      // not nfts to authenticate
+      if (notAuthenticated == false) {
+        process.send('nfts already authenticated')
+        process.exit();
 
-			// or authenticate one of the unauthenticated nfts
-			} else if (notAuthenticated == true) {
+      // or authenticate one of the unauthenticated nfts
+      } else if (notAuthenticated == true) {
 
-				const listen = path.resolve('listenForTransfer.js');
-				const listenChild = fork(listen);
+        const listen = path.resolve('listenForTransfer.js');
+        const listenChild = fork(listen);
 
-				listenChild.send({amount: message.amount, wallet: message.wallet});
-				listenChild.on('message', message => {
-					console.log('status:', message);
-					if (message == 'wallet authenticated') {
-						const set = path.resolve('setAuthenticated.js');
-						const setChild = fork(set);
-						setChild.send({id: notAuthenticatedId});
-						
-						setChild.on('message', message => {
-							if (message == 'nft authenticated') {
-								process.send({
-									type: "authentication complete",
-									wallet: message.wallet,
-									id: notAuthenticatedId
-								});
-								process.exit();
-							};
-						});
+        listenChild.send({amount: message.amount, wallet: message.wallet});
+        listenChild.on('message', message => {
+          console.log('status:', message);
+          if (message == 'wallet authenticated') {
+            const set = path.resolve('setAuthenticated.js');
+            const setChild = fork(set);
+            setChild.send({id: notAuthenticatedId});
+            
+            setChild.on('message', message => {
+              if (message == 'nft authenticated') {
+                process.send({
+                  type: "authentication complete",
+                  wallet: message.wallet,
+                  id: notAuthenticatedId
+                });
+                process.exit();
+              };
+            });
 
-					};
-				});
-			}
-		}
+          };
+        });
+      }
+    }
 
-	} catch(error) {
+  } catch(error) {
 
-		console.log(error);
-		process.send('no nfts present');
-		process.exit();
-	}
+    console.log(error);
+    process.send('no nfts present');
+    process.exit();
+  }
 }
 
 process.on('message', message => {
