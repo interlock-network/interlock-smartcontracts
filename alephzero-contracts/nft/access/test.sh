9c94e9c903c90b13c2b5e4e1335c0ef81c595211
Blair <blairmunroakusa@wp.computer>
2023-03-08 17:52:36 -0900

    access/test.sh: clean up formatting

--- alephzero-contracts/nft/access/test.sh
+++ alephzero-contracts/nft/access/test.sh
@@ -5,25 +5,35 @@ set -x
 
 # deploy and get token contract address
 ADDRESS=$(
-
 	# answer dry-run prompt
 	yes |
-
 	# instantiate token contract with salt
-	cargo contract instantiate --constructor new_token --manifest-path=../../ilockmvp/Cargo.toml --suri //Alice --salt $(date +%s) |
-
+	cargo contract instantiate \
+		--constructor new_token \
+		--manifest-path=../../ilockmvp/Cargo.toml \
+		--suri //Alice \
+		--salt $(date +%s) |
 	# find output line with token contract address
 	grep "Contract " |
-
 	# get address string
 	awk -F " " '{
 		print $2;
 	}';
-
 # assign address string to $ADDRESS
 )
 
 # deploy UANFT contract
-yes | cargo contract instantiate --manifest-path=Cargo.toml --suri //Alice --args '"Interlock-Network-Universal-Access-NFT".to_string' '"ILOCK-UANFT".to_string()' '"USERPASS-ACCESS".to_string()' 10000 100 $ADDRESS --salt $(date +%s)
+yes |
+cargo contract instantiate \
+	--manifest-path=Cargo.toml \
+	--suri //Alice \
+	--args \
+		'"Interlock-Network-Universal-Access-NFT".to_string' \
+		'"ILOCK-UANFT".to_string()' \
+		'"USERPASS-ACCESS".to_string()' \
+		10000 \
+		100 \
+		$ADDRESS \
+	--salt $(date +%s)
 
 
