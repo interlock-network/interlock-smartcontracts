7067d194883363a8c574877035f1aabeab56af52
Blair <blairmunroakusa@wp.computer>
2023-03-14 13:21:37 -0800

    ilockmvp/lib.rs: breakout unit tests

--- contract_ilockmvp/lib.rs
+++ contract_ilockmvp/lib.rs
@@ -10,21 +10,21 @@
 //!
 //! #### To ensure build with cargo-contract version 2.0.0, run:
 //!
-//! -     cargo install cargo-contract --force --version 2.0.0
+//! cargo install cargo-contract --force --version 2.0.0
 //!
 //! #### To build, run:
 //!
-//! -     cargo +nightly contract build
+//! cargo +nightly contract build
 //!
 //! #### To build docs, run:
 //!
-//! -     cargo +nightly doc --no-deps --document-private-items --open
+//! cargo +nightly doc --no-deps --document-private-items --open
 //!
 //! #### To reroute docs in Github, run:
 //!
-//! -     echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" >
-//! -     target/doc/index.html;
-//! -     cp -r target/doc ./docs
+//! echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" >
+//! target/doc/index.html;
+//! cp -r target/doc ./docs
 //!
 
 #![doc(
@@ -85,10 +85,10 @@ pub mod ilockmvp {
 
     #[derive(Debug)]
     pub struct PoolData<'a> {
-        name: &'a str,
-        tokens: u128,
-        vests: u8,
-        cliffs: u8,
+        pub name: &'a str,
+        pub tokens: u128,
+        pub vests: u8,
+        pub cliffs: u8,
     }
 
     /// - Pool data.
@@ -157,16 +157,16 @@ pub mod ilockmvp {
 
         /// - What are the current balances of all the vesting pools?
         /// - This includes the rewards pool balance.
-        balances: [Balance; POOL_COUNT],
+        pub balances: [Balance; POOL_COUNT],
 
         /// - How much ILOCK is circulating right now?
         /// - This includes token held by liquidity pools/exchanges.
         /// - This is the value of `total_supply()` getter.
-        circulating: Balance,
+        pub circulating: Balance,
 
         /// - How much do we have available in collected taxes/fees from port owners
         /// and application contract operators?
-        proceeds: Balance,
+        pub proceeds: Balance,
 
         /// - Expand storage related to the pool accounting functionality.
         pub _reserved: Option<()>,
@@ -312,33 +312,33 @@ pub mod ilockmvp {
         /// - This codehash is essential to making sure that only safe and approved application
         /// contracts are able to connect to this token contract and manipulate its owneronly
         /// functionalities (as defined per respective port protocol).
-        application: Hash,
+        pub application: Hash,
 
         /// - How much does Interlock tax transaction taking place within a port protocol's
         /// socket call?
-        tax: Balance,
+        pub tax: Balance,
 
         /// - For withdrawing rewards from ILOCK rewards pool, what is the max this particular
         /// port owner's application type can withdraw from rewards pool?
-        cap: Balance,
+        pub cap: Balance,
 
         /// - If locked, only Interlock token contract owner can create a socket connection with
         /// this token contract using the appropriate application codehash.
-        locked: bool,
+        pub locked: bool,
 
         /// - How much ILOCK has this port been rewarded or issued throughout the course of
         /// its operation (in case where protocol rewards or issues ILOCK, that is)?
-        paid: Balance,
+        pub paid: Balance,
 
         /// - How much has Interlock collected from this port in taxes or other collections?
-        collected: Balance,
+        pub collected: Balance,
 
         /// - Who is the overall owner of this port?
         /// - Socket operators are not necessarily owners of the port.
         /// - For example, a restaurant franchise has one owner, whereas the franchise may have
         /// numberous restaurant locations, each with it's own operator, each operator/franchise
         /// pair forming a separate socket connection.
-        owner: AccountId,
+        pub owner: AccountId,
     }
     /// - Ink 4 has no AccountId Default impl thus struct Default cannot be derived
     /// due to `owner` field.
@@ -385,7 +385,7 @@ pub mod ilockmvp {
         /// agreement between the restaurant operator and the franchise owner.
         /// - There is only one agreement between the franchise and the restaurant operator,
         /// regardless of how many restaurants the operator has.
-        operator: AccountId,
+        pub operator: AccountId,
 
         /// - What port is this operator connected to?
         /// - Using the restaurant franchise metaphor again, the port is like the franchise
@@ -393,7 +393,7 @@ pub mod ilockmvp {
         /// - The port number is what identifies a particular franchise and its protocols,
         /// procedures, metadata, and ultimately business model and standards for any
         /// franchisees.
-        portnumber: u16,
+        pub portnumber: u16,
     }
     /// - Ink 4 has no AccountId Default impl thus struct Default cannot be derived
     /// due to `operator` field.
@@ -416,31 +416,31 @@ pub mod ilockmvp {
 
         /// - Openbrush PSP22.
         #[storage_field]
-        psp22: psp22::Data,
+        pub psp22: psp22::Data,
 
         /// - Openbrush ownership extension.
         #[storage_field]
-        ownable: ownable::Data,
+        pub ownable: ownable::Data,
 
         /// - Openbrush metadata extension.
         #[storage_field]
-        metadata: metadata::Data,
+        pub metadata: metadata::Data,
 
         /// - ILOCK Rewards info.
         #[storage_field]
-        reward: RewardData,
+        pub reward: RewardData,
 
         /// - ILOCK token pool info.
         #[storage_field]
-        pool: TokenPools,
+        pub pool: TokenPools,
 
         /// - ILOCK vesting info.
         #[storage_field]
-        vest: VestData,
+        pub vest: VestData,
 
         /// - ILOCK connecting application contract info
         #[storage_field]
-        app: AppData,
+        pub app: AppData,
     }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -451,28 +451,28 @@ pub mod ilockmvp {
     #[ink(event)]
     pub struct Transfer {
         #[ink(topic)]
-        from: Option<AccountId>,
+        pub from: Option<AccountId>,
         #[ink(topic)]
-        to: Option<AccountId>,
-        amount: Balance,
+        pub to: Option<AccountId>,
+        pub amount: Balance,
     }
 
     /// - Specify approval event.
     #[ink(event)]
     pub struct Approval {
         #[ink(topic)]
-        owner: Option<AccountId>,
+        pub owner: Option<AccountId>,
         #[ink(topic)]
-        spender: Option<AccountId>,
-        amount: Balance,
+        pub spender: Option<AccountId>,
+        pub amount: Balance,
     }
 
     /// - Specify reward event.
     #[ink(event)]
     pub struct Reward {
         #[ink(topic)]
-        to: Option<AccountId>,
-        amount: Balance,
+        pub to: Option<AccountId>,
+        pub amount: Balance,
     }
 
     /// - Other contract error types.
@@ -2372,155 +2372,8 @@ pub mod ilockmvp {
             Ok(())
         } 
     }
-
-////////////////////////////////////////////////////////////////////////////
-//// unit tests ////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-
-    #[cfg(test)]
-    mod tests {
-
-        use ink::primitives::Hash;
-
-        use super::*;
-
-        /// - Test if the default constructor does its job
-        /// - and check months_passed()
-        /// - and check cap().
-        #[ink::test]
-        fn new_token_works() {
-
-            let ILOCKmvpPSP22 = ILOCKmvp::new_token();
-
-            assert_eq!(ILOCKmvpPSP22.vest.monthspassed, ILOCKmvpPSP22.months_passed());
-            assert_eq!(ILOCKmvpPSP22.vest.nextpayout, ILOCKmvpPSP22.env().block_timestamp() + ONE_MONTH);
-            assert_eq!(ILOCKmvpPSP22.total_supply(), 0);
-            assert_eq!(ILOCKmvpPSP22.metadata.name, Some("Interlock Network".as_bytes().to_vec()));
-            assert_eq!(ILOCKmvpPSP22.metadata.symbol, Some("ILOCK".as_bytes().to_vec()));
-            assert_eq!(ILOCKmvpPSP22.metadata.decimals, 18);
-
-            // this checks that token numbers have been entered accurately into POOLS PoolData
-            let mut total_tokens: u128 = 0;
-            for pool in 0..POOL_COUNT {
-
-                total_tokens += POOLS[pool].tokens * DECIMALS_POWER10;
-            }
-            assert_eq!(total_tokens, ILOCKmvpPSP22.cap());
-            assert_eq!(ILOCKmvpPSP22.ownable.owner, ILOCKmvpPSP22.env().caller());
-        }
-
-        /// HAPPY REGISTER_STAKEHOLDER & STAKEHOLDER_DATA
-        /// - Test if register_stakeholder and stakeholder_data functions works correctly.
-        /// - Registration should succeed as long as stakeholder share > 0.
-        /// - Payremaining should accurately reflect distribution to stakeholder given share.
-        #[ink::test]
-        fn happyunit_register_stakeholder_data() {
-
-        }    
-
-        /// HAPPY POOL_DATA AND POOL_BALANCE
-        /// - Test if pool_data getter does its job.
-        /// - Test if pool_balance does its job.
-        #[ink::test]
-        fn happyunit_pool_data_and_balance() {
-
-            let ILOCKmvpPSP22 = ILOCKmvp::new_token();
-            let pool = &POOLS[1];
-            assert_eq!(ILOCKmvpPSP22.pool_data(1), (
-                format!("pool: {:?} ", pool.name.to_string()),
-                format!("tokens alotted: {:?} ", pool.tokens),
-                format!("number of vests: {:?} ", pool.vests),
-                format!("vesting cliff: {:?} ", pool.cliffs),
-            ));
-        }
-
-        /// HAPPY CREATE_GET_PORT
-        /// - Test if create_port() and port() functions correctly.
-        /// - Test if tax_port_transfer() functions correctly.
-        #[ink::test]
-        fn happyunit_create_get_port_tax_transfer() {
-
-            let mut ILOCKmvpPSP22 = ILOCKmvp::new_token();
-            let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
-
-            let codehash: Hash = Default::default(); // offchain environment doesn't support
-            let tax: Balance = 1_000; // 10% tax      // .own_code_hash()
-            let cap: Balance = 1_000_000;
-            let locked: bool = true;
-            let number: u16 = 2;
-            let owner: AccountId = accounts.bob;
-
-            let _ = ILOCKmvpPSP22.create_port(
-                codehash,
-                tax,
-                cap,
-                locked,
-                number,
-                owner,
-            );
-
-            let mut port: Port = ILOCKmvpPSP22.port(number);
-
-            assert_eq!(port, Port {
-                application: codehash,
-                tax: tax,
-                cap: cap,
-                locked: locked,
-                paid: 0,
-                collected: 0,
-                owner: owner,
-            });
-
-            ILOCKmvpPSP22.pool.circulating += 1_000_000;
-
-            let test_socket: Socket = Socket {
-
-                operator: accounts.eve,
-                portnumber: 2,
-            };
-
-            let _ = ILOCKmvpPSP22.tax_port_transfer(
-                test_socket,
-                port,
-                cap,
-            );
-
-            port = ILOCKmvpPSP22.app.ports.get(number).unwrap();
-
-            assert_eq!(port.paid, 1_000_000 - 1_000); // 999_000
-            assert_eq!(port.collected, 0 + 1_000);
-            assert_eq!(ILOCKmvpPSP22.proceeds_available(), 0 + 1_000);
-            assert_eq!(ILOCKmvpPSP22.total_supply(), 1_000_000 - 1_000);
-        }
-
-        /// SAD TAX_PORT_TRANSFER
-        /// - Not sure there is much to do here.
-        #[test]
-        fn sadunit_tax_port_transfer() {
-        }
-
-/*************************  THIS TEST IS SLOW, THUS COMMENTED OUT UNLESS NEEDED
-
-        /// HAPPY CHECK_TIME
-        /// - Test to make sure month increment doesn't happen too soon.
-        #[ink::test]
-        fn happyunit_check_time() {
-
-            let mut ILOCKmvpPSP22 = ILOCKmvp::new_token();
-
-            for _time in 0..432_000_001 { // number of advances needed to span month
-
-                ink::env::test::advance_block::<ink::env::DefaultEnvironment>();
-            }
-            let timestamp: Timestamp = ink::env::block_timestamp::<ink::env::DefaultEnvironment>();
-
-            assert!(ILOCKmvpPSP22.vest.nextpayout < timestamp);
-            assert_eq!(ILOCKmvpPSP22.vest.monthspassed, 0);
-            let _ = ILOCKmvpPSP22.check_time();
-            assert_eq!(ILOCKmvpPSP22.vest.monthspassed, 1);
-        }
-
-**************************/
-    }
 }
 
+
+#[cfg(test)]
+pub mod tests_unit;
