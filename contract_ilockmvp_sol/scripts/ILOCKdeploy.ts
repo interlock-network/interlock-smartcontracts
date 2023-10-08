041df1d85170d1bdb83167a463f217222dda697d
Blair <blairmunroakusa@wp.computer>
2023-10-08 10:30:26 -0800

    ILOCKdeploy.ts: use .env & produce tx receipt

--- contract_ilockmvp_sol/scripts/ILOCKdeploy.ts
+++ contract_ilockmvp_sol/scripts/ILOCKdeploy.ts
@@ -1,11 +1,28 @@
 import { ethers, upgrades } from "hardhat";
 
+import * as dotenv from "dotenv";
+dotenv.config({ path: './.env.dev' });
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT = process.env.CONTRACT;
+
 async function main () {
-  const ILOCKV1 = await ethers.getContractFactory('ILOCKV1');
-  console.log('Deploying ILOCKV1 token...');
+
+  const ILOCKV1 = await ethers.getContractFactory(CONTRACT);
+
+  console.log('Deploying ILOCK token contract...');
   const ilockv1 = await upgrades.deployProxy(ILOCKV1, { initializer: 'initialize' });
-  await ilockv1.waitForDeployment()
-  console.log('ILOCKV1 token deployed to: ', ilockv1.target);
+  const response = await ilockv1.waitForDeployment()
+  console.log('ILOCKV1 token contract deployed.');
+
+  const receipt = {
+	  "contractNetwork": "Arbitrum Mainnet",
+		"contractAddress": response.target
+  };
+  const deploymentReceipt = {
+	  "deploymentReceipt": receipt
+  };
+  console.log(deploymentReceipt)
 }
 
 main().catch((error) => {
