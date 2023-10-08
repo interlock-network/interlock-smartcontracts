e7d7d2f0ad3f22cf2a8504cf43a54b2d89e94e7d
Blair <blairmunroakusa@wp.computer>
2023-10-07 17:11:28 -0800

    add, .env.dev, getStakIdentifiers.ts, generate test stakes

--- contract_ilockmvp_sol/scripts/getStakeIdentifiers.ts
+++ contract_ilockmvp_sol/scripts/getStakeIdentifiers.ts
@@ -1,20 +1,23 @@
 import { ethers as hardhatEthers, upgrades } from "hardhat";
 import { ethers } from "ethers";
 
+require("dotenv").config();
+
 import * as dotenv from "dotenv";
 dotenv.config({ path: './.env.dev' });
 
-const CONTRACT = process.env.CONTRACT_ADDRESS;
+const CONTRACT = 'ILOCKV1';
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
 
 async function main () {
-  const ILOCKV1 = await hardhatEthers.getContractFactory('ILOCKV1');
-  console.log(CONTRACT);
-  const ilockv1 = await ILOCKV1.attach(CONTRACT);
+  const ILOCKV1 = await hardhatEthers.getContractFactory(CONTRACT);
+  const ilockv1 = await ILOCKV1.attach(CONTRACT_ADDRESS);
 
   const stakeholder = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
   const paid = 0;
   const share = ethers.parseEther("100000");
-  const pool = 2;
+  const pool = 1;
 
   const data = {
     paid: paid,
@@ -22,7 +25,7 @@ async function main () {
     pool: pool
   };
 
-  await ilockv1.registerStake(stakeholder, data);
+  console.log(await ilockv1.getStakeIdentifiers(stakeholder));
   //console.log((await ilockv1.newstorage()).toString())
 }
 
