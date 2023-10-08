c498ead7d2408b382b61871fdddc2e69f074872b
Blair <blairmunroakusa@wp.computer>
2023-10-08 10:17:59 -0800

    use .env & include receipt

--- contract_ilockmvp_sol/scripts/ILOCKupgrade.ts
+++ contract_ilockmvp_sol/scripts/ILOCKupgrade.ts
@@ -1,12 +1,26 @@
 import { ethers, upgrades } from "hardhat";
 
+import * as dotenv from "dotenv";
+dotenv.config({ path: './.env.dev' });
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_UPGRADE = process.env.CONTRACT_UPGRADE;
 
 async function main () {
-  const ILOCKV2 = await ethers.getContractFactory('ILOCKV2');
-  console.log('Upgrading ILOCK...');
-  const result = await upgrades.upgradeProxy('0x51A1ceB83B83F1985a81C295d1fF28Afef186E02', ILOCKV2);
-  console.log('ILOCK upgraded');
-  console.log(result)
+  const ILOCKV2 = await ethers.getContractFactory(CONTRACT_UPGRADE);
+
+  console.log('Upgrading ILOCK token contract...');
+  const response = await upgrades.upgradeProxy(CONTRACT_ADDRESS, ILOCKV2);
+  console.log('ILOCK token contract upgraded.');
+
+  const receipt = {
+	  "upgradeHash": response.deployTransaction.hash,
+	  "upgradeBlockHash": response.deployTransaction.blockHash
+  };
+  const upgradeReceipt = {
+	  "upgradeReceipt": receipt
+  };
+  console.log(upgradeReceipt);
 }
 
 main().catch((error) => {
