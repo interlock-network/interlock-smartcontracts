8de3ba67a6f9f70405293e44c2fbe62da10e2155
Blair <blairmunroakusa@wp.computer>
2023-10-08 18:55:22 -0800

    getStakes.ts placeholder

--- contract_ilockmvp_sol/scripts/validateStakes.ts
+++ contract_ilockmvp_sol/scripts/validateStakes.ts
@@ -7,11 +7,65 @@ dotenv.config({ path: './.env.dev' });
 const CONTRACT = process.env.CONTRACT;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const STAKE_DATA = JSON.parse(readFileSync(process.env.STAKE_DATA).toString());
+const IDENTIFIER_LOG_DATA = JSON.parse(readFileSync(process.env.IDENTIFIER_LOG_PATH).toString());
+const STAKE_LOG_DATA = JSON.parse(readFileSync(process.env.STAKE_LOG_PATH).toString());
 
 async function main () {
-
   const ILOCKV1 = await ethers.getContractFactory(CONTRACT);
   const ilockv1 = await ILOCKV1.attach(CONTRACT_ADDRESS);
+
+	let stakeholders = []
+  for (stake of STAKE_DATA.stakes) {
+
+					if (!stakeholders.includes(stake.stakeholder)) {
+									stakeholders.push(stake.stakeholder);
+					}
+	}
+	if (stakeholders.length != STAKE_DATA.stakeholders.length) {
+					console.log('registered stakeholders are either missing or contain extra stakes');
+					process.exitCode = 1;
+	}
+
+	let loggedStakes = []
+	const stakeholderStakes = STAKE_LOG_DATA.stakeholderStakes;
+	for (stakeholder of stakeholderStakes) {
+					if (!loggedStakes.includes(stakes.identifier)) {
+									console.log('missing stakeholder');
+					}
+
+    
+
+
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * This is to do.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
 
   let stakeholderIdentifiers = [];
   for (const stakeholder of STAKE_DATA.stakeholders) {
