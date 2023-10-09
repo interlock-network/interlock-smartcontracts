9a894f583e975f1d0f7df44562d5f94611d3c5b7
Blair <blairmunroakusa@wp.computer>
2023-10-09 08:46:33 -0800

    enableMultisig.ts: impl (for connecting multisig Safe account)

---
+++ contract_ilockmvp_sol/scripts/enableMultisig.ts
@@ -0,0 +1,35 @@
+import { upgrades } from "hardhat";
+import { readFileSync, writeFileSync } from "fs";
+
+import * as dotenv from "dotenv";
+dotenv.config({ path: './.env.dev' });
+
+const SAFE_ADDRESS = process.env.SAFE_ADDRESS;
+const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
+const ADMIN_LOG_PATH = process.env.ADMIN_LOG_PATH;
+
+async function main () {
+
+  console.log('Transferring admin contract ownership to Safe multisig account...');
+  await upgrades.admin.transferProxyAdminOwnership(SAFE_ADDRESS);
+  console.log('Admin contract ownership transferred to Safe multisig account.');
+
+  let multisigEnabledReceipt = {
+    "oldAddress": OWNER_ADDRESS,
+    "safeAddress": SAFE_ADDRESS,
+    "dateAndTime": new Date().toUTCString()
+  };
+  multisigEnabledReceipt = {
+    "multisigEnabledReceipt": multisigEnabledReceipt
+  };
+
+  console.log(multisigEnabledReceipt);
+  const buffer = JSON.parse(readFileSync(ADMIN_LOG_PATH, 'utf8'));
+  buffer.push(multisigEnabledReceipt);
+  writeFileSync(ADMIN_LOG_PATH, JSON.stringify(buffer, null, 2), 'utf-8');
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
