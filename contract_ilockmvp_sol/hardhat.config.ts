ecb84cd98ae52a8cc323fe9251f20afe071fe2ef
Blair <blairmunroakusa@wp.computer>
2023-10-12 14:52:21 -0800

    hardhat.config.ts: add base-goerli network

--- contract_ilockmvp_sol/hardhat.config.ts
+++ contract_ilockmvp_sol/hardhat.config.ts
@@ -7,6 +7,12 @@ import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  // for testnet
+  "base-goerli": {
+    url: "https://goerli.base.org",
+    accounts: [process.env.PRIVATE_KEY as string]
+    gasPrice: 1000000000,
+  },
 };
 
 export default config;
